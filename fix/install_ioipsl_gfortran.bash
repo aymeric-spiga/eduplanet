5956974987e80278f33748abb4bfaf5c73a8010f
Aymeric Spiga <aymeric.spiga@upmc.fr>
2014-08-27 15:38:41 +0200

    commit to install ioipsl with bash and not ksh.

---
+++ fix/install_ioipsl_gfortran.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+# script to download and install the latest version of IOIPSL
+# using gfortran
+# You'll probably have to change paths to NetCDF library 'lib' and 'include'
+# below to adapt this script to your computer.
+
+setfolder="/home3/spiga/3T054/eduplanet/MODELES/LMDZ.COMMON/netcdf/gfortran_netcdf-4.0.1"
+#setfolder="/donnees/emlmd/netcdf64-4.0.1_gfortran/"
+
+#0. Preliminary stuff 
+# netcdf include and lib dirs:
+netcdf_include=$setfolder"/include"
+netcdf_lib=$setfolder"/lib"
+
+whereami=`pwd -P`
+
+# 1. Get IOIPSL (via modipsl)
+svn co http://forge.ipsl.jussieu.fr/igcmg/svn/modipsl/trunk modipsl
+cd modipsl/util
+
+find * -exec sed -i s:ksh:bash:g {} \;
+./model IOIPSL
+
+# 2. Set correct settings:
+# modify path to netcdf in 'AA_make.gdef'
+cp AA_make.gdef AA_make.gdef.old
+sed -e s:"gfortran  NCDF_INC = /usr/local/include":"gfortran  NCDF_INC = ${netcdf_include}":1 \
+    -e s:"gfortran  NCDF_LIB = -L/usr/local/lib":"gfortran  NCDF_LIB = -L${netcdf_lib}":1 \
+    AA_make.gdef.old > AA_make.gdef
+
+# set default working precision for IOIPSL:
+./ins_make -t gfortran -p I4R8
+
+## 3. build ioipsl:
+cd ../modeles/IOIPSL/src
+
+find * -exec sed -i s:ksh:bash:g {} \;
+make
+
+if [[ -f ${whereami}/modipsl/lib/libioipsl.a ]] 
+  then
+  echo "OK: ioipsl library is in ${whereami}/modipsl/lib"
+else
+  echo "Something went wrong..."
+fi
+
