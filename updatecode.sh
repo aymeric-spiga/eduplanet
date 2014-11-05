be90c76c0c02d639972ee1dcf365270b74fa6217
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2014-11-05 13:34:31 +0100

    added in updatecode the download of earth and mars surface properties

--- updatecode.sh
+++ updatecode.sh
@@ -12,3 +12,13 @@ echo "*** update planetoplot"
 cd planetoplot
 git pull
 cd ..
+
+echo "*** get supplementary files"
+cd RUN/DATAGENERIC
+if [[ ! (-f "surface_earth.nc") ]] ; then
+  wget "http://www.lmd.jussieu.fr/~aslmd/planetoplot/surface_earth.nc"
+fi
+if [[ ! (-f "surface_mars.nc") ]] ; then
+  wget "http://www.lmd.jussieu.fr/~aslmd/planetoplot/surface_mars.nc"
+fi
+cd ../..
