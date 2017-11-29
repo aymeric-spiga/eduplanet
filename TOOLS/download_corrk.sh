b18a3e4b52c8ee95d910eea0ed107d9361358a65
mturbet <martin.turbet@lmd.jussieu.fr>
2017-11-29 14:17:15 +0100

    Create download_corrk.sh

---
+++ TOOLS/download_corrk.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "*** download all the correlated-k absorption coefficient databases"
+cd RUN/DATAGENERIC/corrk/
+
+if [[ ! (-f "megaCO2") ]] ; then
+  wget "http://www.lmd.jussieu.fr/~mturbet/eduplanet/corrk/megaCO2"
+fi
+cd ../../..
