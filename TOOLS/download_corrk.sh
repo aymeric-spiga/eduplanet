ec69aabb8589bd6ef640cb3b661a639de7a9354d
mturbet <martin.turbet@lmd.jussieu.fr>
2017-11-29 23:17:43 +0100

    Update download_corrk.sh

--- TOOLS/download_corrk.sh
+++ TOOLS/download_corrk.sh
@@ -4,7 +4,7 @@ echo "*** download all the correlated-k absorption coefficient databases"
 cd ../RUN/DATAGENERIC/corrk_data/
 
 if [[ ! (-f "megaCO2") ]] ; then
-  wget "http://www.lmd.jussieu.fr/~mturbet/eduplanet/corrk/megaCO2.zip"
+  wget "http://www.lmd.jussieu.fr/~mturbet/eduplanet/corrk_data/megaCO2.zip"
 fi
 unzip megaCO2.zip
 rm -f megaCO2.zip
