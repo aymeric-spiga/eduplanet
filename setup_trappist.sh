9e8074d201ee452e3a587028a408f8f876157487
alpha-jnaar <80282665+alpha-jnaar@users.noreply.github.com>
2021-04-01 11:24:32 +0200

    Add files via upload

---
+++ setup_trappist.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+thisfolder=$PWD
+
+
+echo "Hello student !"
+echo "You are on "`hostname`
+echo "You are about to change settings in order to get starting configuration for a slab ocean earth :"
+echo "Do you want to continue ? (1=YES, 0=NO)"
+read answer
+
+if [ $answer -eq 1 ]
+then
+  cp $thisfolder/INIT/DATAGENERIC/stellar_spectra/Flux_TRAPPIST1.txt $thisfolder/INIT/DATAGENERIC/stellar_spectra/Flux_TRAPPIST1.dat
+  cp $thisfolder/INIT/DATAGENERIC/stellar_spectra/lambda_TRAPPIST1.txt $thisfolder/INIT/DATAGENERIC/stellar_spectra/lambda_TRAPPIST1.dat
+  echo "Obscur setting changes :"
+  cp $thisfolder/INIT/newstart.F.ocean $thisfolder/MODELES/LMDZ.GENERIC/libf/dynphy_lonlat/phystd/newstart.F
+  cp $thisfolder/INIT/iniphy.ocean $thisfolder/MODELES/LMDZ.GENERIC/libf/dynphy_lonlat/phystd/iniphysiq_mod.F90
+  echo "Done !"
+  echo "Setting reglages_init.txt :"
+  rm $thisfolder/INIT/planet_start
+  ln -s $thisfolder/INIT/planet_start.trappist $thisfolder/INIT/planet_start
+  echo "Done !"
+  echo "Setting reglages_run.txt :"
+  cp $thisfolder/RUN/etu.def.trappist $thisfolder/RUN/etu.def
+  echo "Done !"
+  echo "Setting reglages_compiler.txt :"
+  cp $thisfolder/INIT/compiler.trappist $thisfolder/reglages_compiler.txt
+  echo "Done !"
+fi
